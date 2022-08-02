bdcb77e8d809400038f9e57eeabc043603a668cf
tarmon329 <tarmon329@gmail.com>
2022-08-02 16:52:26 +0300

    404  route fix

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ const App = () => {
       <DarkMode />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="404" element={<FOF />} />
-        <Route path="/*" element={<Navigate to="404" replace />} />
+        <Route path="/404" element={<FOF />} />
+        <Route path="/*" element={<Navigate to="/404" replace />} />
         <Route path="/characters" element={<Characters />} />
         <Route path="/characters/:id" element={<Character />} />
         <Route path="/episode/:id" element={<Episode />} />
